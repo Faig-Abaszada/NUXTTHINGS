d277fd631edfa3884cfe0d029fa3df0b4f25a1d8
Faig <faik.abaszada@gmail.com>
2024-04-02 11:58:26 +0400

    Update navigation links and add static link

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-<h1>Table component</h1>
+<div>
+  <div>
+    <ul>
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/about">About</NuxtLink></li>
+      <li><NuxtLink to="/products">Products</NuxtLink></li>
+    </ul>
+
+    <!-- 
+      main difference is between nuxtlink and a tag 
+      it make a fresh request to server and reload the page
+     -->
+    <a href="/about">Static link</a>
+  </div>
   <table-component
       :is-static-mode="true"
       :columns="defineTableColumns"
@@ -11,6 +24,7 @@
       :is-total-on-top="true"
       :max-height="285"
   ></table-component>
+</div>
 </template>
 <script>
 // import { defineAsyncComponent } from 'vue'
